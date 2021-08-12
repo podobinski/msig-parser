7c7566898b231e29c9925dc949846863b27164d5
podobinski <piotr.podobinski@gmail.com>
2021-08-12 11:02:46 +0200

    Wersja 0.001

---
+++ msig-parser.py
@@ -0,0 +1,23 @@
+import os
+import datetime
+
+plik = open("monitor-export.txt")
+plik_nowy = open("monitor-etap1.txt", "w+")
+linia_nowa = ""
+
+print ("Rozpoczęto przetwarzanie MSiG")
+
+for linia in plik.readlines():  
+    if "PESEL" in linia:
+        linia_nowa = linia[linia.find("PESEL")+6:linia.find("PESEL")+17]
+        plik_nowy.write(linia_nowa)
+        plik_nowy.write("\n")
+    else:
+        pass             
+
+plik_nowy.close()                       
+plik.close()
+print ("Zakończono przetwarzanie MSiG")
+input ("Naciśnij dowolny przycisk aby zakończyć")
+
+#wersja 0.001
